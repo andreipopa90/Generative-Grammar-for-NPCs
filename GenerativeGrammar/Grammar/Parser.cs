872548f749173436aea337333f8768b636dc503c
Andrei Popa <popa.andrey90@yahoo.com>
2022-05-05 21:04:36 +0200

    Small update

---
+++ GenerativeGrammar/Grammar/Parser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace GenerativeGrammar.Grammar
+{
+	public class Parser
+	{
+		public Parser()
+		{
+		}
+
+		public void ReadGrammarFile()
+		{
+			string[] lines = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"", "Grammar", "Grammar.txt"));
+			Console.Write(lines);
+		}
+
+		static void Main()
+		{
+			Parser parser = new();
+			parser.ReadGrammarFile();
+			Console.WriteLine("Hello, world!");
+		}
+	}
+}
